6e9272930c4d40a95be1ac608ddb374f9933a5aa
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-09 11:45:48 -0700

    setup project folder and dummy component

---
+++ src/components/TableList/TableList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./TableList.css";
+
+const TableList = (props) => {
+    return (
+        <div>
+            Table goes here
+        </div>
+    )
+}
+
+export default TableList;
