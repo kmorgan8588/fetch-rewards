eb1f9d3f45f7e77dc00038fd3475b8fa76f93443
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-09 14:34:05 -0700

    setup util queryApi func to query a url and rebased useEffects in Table to align with the process of calling the func on mount

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,35 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import "./Table.css";
-import TESTDATA from '../../TestData';
 import TableList from '../TableList/TableList';
 import SelectPage from '../SelectPages/SelectPage';
+import queryFunc from '../../util/queryApi';
+
 
 const Table = (props) => {
-    const { filterFunc } = props;
+    const { filterFunc, url } = props;
     const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [pageNumber, setPageNumber] = useState(1)
 
     useEffect(() => {
-        const filteredData = filterFunc(TESTDATA);
-        setData(filteredData);
-    }, [filterFunc])
+        const cleanData = filterFunc(data);
+        setFilteredData(cleanData);
+    }, [filterFunc, data])
 
     useEffect(() => {
-        setTimeout(() => setIsLoading(false), 1000)
-    }, [])
+        const unfilteredData = queryFunc(url);
+        setData(unfilteredData)
+    }, [url])
 
     useEffect(() => {
-        const count = Math.ceil(data.length / 10);
+        const count = Math.ceil(filteredData.length / 10);
         setPageCount(count);
-    }, [data])
+    }, [filteredData])
+
+    useEffect(() => {
+        if (pageCount > 0) {
+            setTimeout(
+                () => setIsLoading(false), 1000)
+        }
+    }, [pageCount])
 
     return (
         <div>
             <h1>Records</h1>
             {isLoading ? null : <SelectPage count={pageCount} update={setPageNumber} />}
-            {isLoading ? "Loading..." : <TableList contents={data.slice((pageNumber - 1) * 10, pageNumber * 10)} />}
+            {isLoading ? "Loading..." : <TableList contents={filteredData.slice((pageNumber - 1) * 10, pageNumber * 10)} />}
         </div>
     )
 }
---
+++ src/util/queryApi.js
@@ -0,0 +1,7 @@
+import TESTDATA from '../TestData';
+
+const queryApi = (url) => {
+    return TESTDATA
+}
+
+export default queryApi;
